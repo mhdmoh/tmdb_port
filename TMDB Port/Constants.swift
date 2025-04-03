600a4d5e50c0d5f587614b028dbd24e926f86ae7
MhdMoh <mhd.nidal.mhd@gmail.com>
2025-04-03 20:43:38 +0200

    setting up project structure

---
+++ TMDB Port/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  TMDB Port
+//
+//  Created by Mohamad Mohamad on 2025. 04. 03..
+//
+
+import Foundation
---
+++ TMDB Port/UI/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  TMDB Port
+//
+//  Created by Mohamad Mohamad on 2025. 04. 03..
+//
+
+import Foundation
